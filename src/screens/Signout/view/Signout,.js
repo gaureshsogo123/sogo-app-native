98fd97b428625870262e047581110dd6fc8773a4
gauresh123 <waranggauresh@gmail.com>
2022-11-22 11:37:45 +0530

    datefilter and bottomnav update

---
+++ src/screens/Signout/view/Signout,.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const Signout = () => {
+  return (
+    <View>
+        <Text>Signout</Text>
+    </View>
+  )
+}
+
+export default Signout
